b246e0d8b5ce3dc1d8d136bfb9db1b24c48319ab
Shafkath Shuhan <shafkathshuhannyc@gmail.com>
2021-03-04 22:32:57 -0500

    create React app

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> StudyBuddy!</h1>
+      <button type="button">Sign In</button>
+    </div>
+  );
+}
+
+export default App;
